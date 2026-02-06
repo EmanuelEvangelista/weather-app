139045e9be41f7b9dc77bfc91dbbdcf9dadb4151
Emanuel Evangelista <emanu.vange@gmail.com>
2026-02-06 18:35:19 -0300

    feat display user name or email in navbar.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,18 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef, useContext } from "react";
 import { NavLink } from "react-router-dom";
 import styles from "./Navbar.module.css";
 import { FaSearch } from "react-icons/fa";
-
 import { FaBars } from "react-icons/fa";
+import { WeatherContext } from "../../context/WeatherContext.jsx";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const { isAuthenticated, user } = useContext(WeatherContext);
   const menuRef = useRef(null);
 
   const toggleMenu = () => setIsOpen(!isOpen);
 
-    useEffect(() => {
+  useEffect(() => {
     function handleClickOutside(event) {
       if (menuRef.current && !menuRef.current.contains(event.target)) {
         setIsOpen(false);
@@ -24,6 +25,8 @@ const Navbar = () => {
     };
   }, []);
 
+  console.log(isAuthenticated, user.firstName);
+
   return (
     <nav className={styles.nav}>
       {/* Botón hamburguesa visible solo en móvil */}
@@ -31,32 +34,59 @@ const Navbar = () => {
         <FaBars size={24} />
       </button>
 
-      <ul ref={menuRef}  className={`${styles.navList} ${isOpen ? styles.open : ""}`}>
+      <ul
+        ref={menuRef}
+        className={`${styles.navList} ${isOpen ? styles.open : ""}`}
+      >
         <li>
-          <NavLink to="/" end onClick={toggleMenu}
-            className={({ isActive }) => (isActive ? styles.active : "")}>
+          <NavLink
+            to="/"
+            end
+            onClick={toggleMenu}
+            className={({ isActive }) => (isActive ? styles.active : "")}
+          >
             WEATHER
           </NavLink>
         </li>
         <li>
-          <NavLink to="/findcity" onClick={toggleMenu}
-            className={({ isActive }) => (isActive ? styles.active : "")}>
+          <NavLink
+            to="/findcity"
+            onClick={toggleMenu}
+            className={({ isActive }) => (isActive ? styles.active : "")}
+          >
             <FaSearch style={{ marginRight: "6px" }} />
             <span>ADD CITY</span>
           </NavLink>
         </li>
         <li>
-          <NavLink to="/help" onClick={toggleMenu}
-            className={({ isActive }) => (isActive ? styles.active : "")}>
+          <NavLink
+            to="/help"
+            onClick={toggleMenu}
+            className={({ isActive }) => (isActive ? styles.active : "")}
+          >
             HELP
           </NavLink>
         </li>
-        <li>
-          <NavLink to="/login" onClick={toggleMenu}
-            className={({ isActive }) => (isActive ? styles.active : "")}>
-            LOGIN
-          </NavLink>
-        </li>
+        {isAuthenticated ? (
+          <li>
+            <NavLink
+              to="/login"
+              className={({ isActive }) => (isActive ? styles.active : "")}
+            >
+              Hello {user.firstName || user.email}
+            </NavLink>
+          </li>
+        ) : (
+          <li>
+            <NavLink
+              to="/login"
+              onClick={toggleMenu}
+              className={({ isActive }) => (isActive ? styles.active : "")}
+            >
+              LOGIN
+            </NavLink>
+          </li>
+        )}
       </ul>
     </nav>
   );
